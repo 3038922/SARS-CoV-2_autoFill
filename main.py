085870a0bee49c5501385abbe469cda9a966e4ad
3038922 <31612534@qq.com>
2020-05-18 13:45:56 +0800

    update .gitignore

--- main.py
+++ main.py
@@ -17,42 +17,45 @@ wb = openpyxl.load_workbook(inputFileName)
 # 显示所有表单
 dict1 = {"总数": 0, "体温异常": 0, "其他异常": 0, "详情": [
     ["班级", "姓名", "是否体温异常", "晨检异常情况描述"]]}
-dict2 = {"总数": 0, "详情": [["班级", "姓名", "接触时间", "学生与接触人员关系", "接触人员去过哪里"]]}
-dict3 = {"总数": 0, "详情": [["班级", "姓名", "接触时间", "学生与接触人员关系", "接触人员去过哪里"]]}
+dict2 = {"总数": 0, "详情": [["班级", "姓名", "文字描述"]]}
+dict3 = {"总数": 0, "详情": [["班级", "姓名", "文字描述"]]}
 dict4 = {"总数": 0, "因发热未到或请假": 0, "因咳嗽腹泄乏力请假人数": 0, "因其他原因请假人数": 0, "详情": [
     ["班级", "姓名", "是否发热", "是否咳嗽乏力腹泻呕吐等", "是否有重点疫区接触史", "请假原因"]]}
 班级 = ""
 晨检午检异常描述 = ""
+乘坐公交车学生人数 =0
+for cell in wb["主表"]["J"]:
+    if(cell.row>2):
+        乘坐公交车学生人数+=int(cell.value)
+
 for cell in wb["晨检异常登记"]["C"]:
     if(cell.value != "") and (cell.value != "姓名"):
         dict1["总数"] += 1
         if(wb["晨检异常登记"].cell(cell.row, 4).value != "否") and (wb["请假学生登记"].cell(cell.row, 4).value != "无"):
             dict1["体温异常"] += 1
-        for it in wb["主表"]["Q"]:
+        for it in wb["主表"]["R"]:
             if (str(it.value).find(cell.value) >= 0):
                 班级 = str(wb["主表"].cell(it.row, 6).value) + \
                     str(wb["主表"].cell(it.row, 7).value)
-        晨检午检异常描述 += str(班级 + cell.value+wb["晨检异常登记"].cell(cell.row, 5).value)
+        晨检午检异常描述 += str(班级 + cell.value+wb["晨检异常登记"].cell(cell.row, 5).value)+"\n"
         dict1["详情"].append(
             [班级, cell.value, wb["晨检异常登记"].cell(cell.row, 4).value, wb["晨检异常登记"].cell(cell.row, 5).value])
 for cell in wb["学生接触外省人员登记"]["C"]:
     if(cell.value != "") and (cell.value != "姓名"):
         dict2["总数"] += 1
-        for it in wb["主表"]["Q"]:
+        for it in wb["主表"]["R"]:
             if (str(it.value).find(cell.value) >= 0):
                 班级 = str(wb["主表"].cell(it.row, 6).value) +\
                     str(wb["主表"].cell(it.row, 7).value)
-        dict2["详情"].append([班级, cell.value, wb["学生接触外省人员登记"].cell(cell.row, 4).value,  wb["学生接触外省人员登记"].cell(
-            cell.row, 5).value,  wb["学生接触外省人员登记"].cell(cell.row, 6).value])
+        dict2["详情"].append([班级, cell.value, wb["学生接触外省人员登记"].cell(cell.row, 4).value])
 for cell in wb["家长接触重点疫区、境外登记"]["C"]:
     if(cell.value != "") and (cell.value != "姓名"):
         dict3["总数"] += 1
-        for it in wb["主表"]["Q"]:
+        for it in wb["主表"]["R"]:
             if (str(it.value).find(cell.value) >= 0):
                 班级 = str(wb["主表"].cell(it.row, 6).value) +\
                     str(wb["主表"].cell(it.row, 7).value)
-        dict3["详情"].append([班级, cell.value, wb["家长接触重点疫区、境外登记"].cell(cell.row, 4).value, wb["家长接触重点疫区、境外登记"].cell(
-            cell.row, 5).value, wb["家长接触重点疫区、境外登记"].cell(cell.row, 6).value])
+        dict3["详情"].append([班级, cell.value, wb["家长接触重点疫区、境外登记"].cell(cell.row, 4).value])
 for cell in wb["请假学生登记"]["C"]:
     if(cell.value != "") and (cell.value != "姓名"):
         dict4["总数"] += 1
@@ -60,7 +63,7 @@ for cell in wb["请假学生登记"]["C"]:
             dict4["因发热未到或请假"] += 1
         if(wb["请假学生登记"].cell(cell.row, 5).value != "否") and (wb["请假学生登记"].cell(cell.row, 5).value != "无"):
             dict4["因咳嗽腹泄乏力请假人数"] += 1
-        for it in wb["主表"]["Q"]:
+        for it in wb["主表"]["R"]:
             if (str(it.value).find(cell.value) >= 0):
                 班级 = str(wb["主表"].cell(it.row, 6).value) +\
                     str(wb["主表"].cell(it.row, 7).value)
@@ -91,10 +94,10 @@ for cell in dict4["详情"]:
 wbNEW["综合统计"].append(
     ["学校名称", "应到人数", "实到人数", "请假学生登记人数总数", "因发热未到或请假", "因咳嗽腹泄乏力请假人数", "因其他原因请假人数",
      "晨检异常人数总数", "晨检体温异常人数", "晨检其他异常人数", "晨检午检异常描述(文字)",
-     "学生接触外省人员登记人数总数", "家长接触重点疫区、境外登记人数总数"])
+     "学生接触外省人员登记人数总数", "家长接触重点疫区、境外登记人数总数","乘坐公交车学生人数"])
 wbNEW["综合统计"].append([学校名称, 应到人数, 应到人数-dict4["总数"], dict4["总数"], dict4["因发热未到或请假"], dict4["因咳嗽腹泄乏力请假人数"], dict4["因其他原因请假人数"],
                       dict1["总数"], dict1["体温异常"], dict1["其他异常"], 晨检午检异常描述,
-                      dict2["总数"], dict3["总数"]])
+                      dict2["总数"], dict3["总数"],乘坐公交车学生人数])
 
 
 wbNEW.save(ouputFilename)
