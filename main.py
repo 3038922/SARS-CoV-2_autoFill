97fcec10137754b9f5bd24a9d6232deb0ee62060
3038922 <31612534@qq.com>
2020-05-18 23:46:42 +0800

    update format
    Signed-off-by: 3038922 <31612534@qq.com>

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main(argv=None):
                     班级 = str(wb["主表"].cell(it.row, 6).value) + \
                         str(wb["主表"].cell(it.row, 7).value)
             晨检午检异常描述 += str(班级 + cell.value +
-                            wb["晨检异常登记"].cell(cell.row, 5).value)+"\n"
+                            wb["晨检异常登记"].cell(cell.row, 5).value) + "\n"
             dict1["详情"].append(
                 [班级, cell.value, wb["晨检异常登记"].cell(cell.row, 4).value, wb["晨检异常登记"].cell(cell.row, 5).value])
     for cell in wb["学生接触外省人员登记"]["C"]:
@@ -72,10 +72,10 @@ def main(argv=None):
                     班级 = str(wb["主表"].cell(it.row, 6).value) +\
                         str(wb["主表"].cell(it.row, 7).value)
             dict4["详情"].append([班级, cell.value, wb["请假学生登记"].cell(cell.row, 4).value, wb["请假学生登记"].cell(
-                cell.row, 5).value, wb["请假学生登记"].cell(cell.row, 6).value,  wb["请假学生登记"].cell(cell.row, 7).value])
+                cell.row, 5).value, wb["请假学生登记"].cell(cell.row, 6).value, wb["请假学生登记"].cell(cell.row, 7).value])
         else:
             wb["请假学生登记"].delete_rows(cell.row)
-    dict1["其他异常"] = dict1["总数"]-dict1["体温异常"]
+    dict1["其他异常"] = dict1["总数"] - dict1["体温异常"]
     dict4["因其他原因请假人数"] = dict4["总数"] - dict4["因发热未到或请假"] - dict4["因咳嗽腹泄乏力请假人数"]
     # EXCEL 新表
     wbNEW = openpyxl.Workbook()
@@ -96,7 +96,7 @@ def main(argv=None):
         ["学校名称", "应到人数", "实到人数", "请假学生登记人数总数", "因发热未到或请假", "因咳嗽腹泄乏力请假人数", "因其他原因请假人数",
          "晨检异常人数总数", "晨检体温异常人数", "晨检其他异常人数", "晨检午检异常描述(文字)",
          "学生接触外省人员登记人数总数", "家长接触重点疫区、境外登记人数总数", "乘坐公交车学生人数"])
-    wbNEW["综合统计"].append([学校名称, 应到人数, 应到人数-dict4["总数"], dict4["总数"], dict4["因发热未到或请假"], dict4["因咳嗽腹泄乏力请假人数"], dict4["因其他原因请假人数"],
+    wbNEW["综合统计"].append([学校名称, 应到人数, 应到人数 - dict4["总数"], dict4["总数"], dict4["因发热未到或请假"], dict4["因咳嗽腹泄乏力请假人数"], dict4["因其他原因请假人数"],
                           dict1["总数"], dict1["体温异常"], dict1["其他异常"], 晨检午检异常描述,
                           dict2["总数"], dict3["总数"], 乘坐公交车学生人数])
     wbNEW.save(ouputFilename)
